ab07f7d9af3b1ef7d31954093fc88dc2e697e08f
Sam Sit <samrat.sitaula@apexcollege.edu.np>
2022-07-22 23:02:01 +0545

    Lab 8,9 - BackEnd Features

--- ecommerce_samrat_sitaula/product_module/views.py
+++ ecommerce_samrat_sitaula/product_module/views.py
@@ -56,64 +56,57 @@ def index(request):
 
 #TODO for CartItem login
 
-
-@login_required(login_url = "/admin/login")
-
+@login_required(login_url="/admin/login")
 def cart(request):
-    #get request data
+    # get request data
     product_id = request.GET.get("id")
     quantity = request.GET.get("qty")
-    
+
     if product_id:
-        #retrieve product data
+        # retrieve product data
         product = Product.objects.get(id=product_id)
-        
         try:
             # get cart item and increase quantity
-            cart_item = CartItem.objects.get(user=request.user, product=product)
+            cart_item = CartItem.objects.get(user=request.user,
+            product=product)
             cart_item.quantity += int(quantity)
             cart_item.entered_on = datetime.now()
-            
         except CartItem.DoesNotExist:
             # initialize cart item
             cart_item = CartItem(
-                user = request.user,
-                product=product,
-                quantity=int(quantity),
-                entered_on = datetime.now(),
+            user=request.user,
+            product=product,
+            quantity=int(quantity),
+            entered_on = datetime.now(),
             )
-        # Save to database
-        
+        # save to database
         cart_item.save()
-        
-    #retrive the cart items for the user from db
-    
     cart_items = CartItem.objects.filter(user=request.user)
-    
-    # calculate total 
     total = 0
     for item in cart_items:
         total += item.product.price * item.quantity
-        
-    # return view
-    
+        # return view
     context = {
-        'cart_items': cart_items,
-        'total': total,
-    }     
-    
+    'cart_items': cart_items,
+    'total': total,
+    }
     return render(request, "cart.html", context)
 
 def removecart(request, id):
     try:
-        # get cart item and remove it
+        # get cart item and increase quantity
         product = Product.objects.get(id=id)
-        cart_item = CartItem.objects.get(user = request.user, product=product)
+        cart_item = CartItem.objects.get(user=request.user, product=product)
         cart_item.delete()
-    
     except CartItem.DoesNotExist:
         pass
-    
-    
-    # redirect to cart 
+    # redirect to cart
     return redirect(cart)
+
+def success_page(request):
+    message = request.session["message"]
+    return render(request, "success.html", {"message": message})
+
+def error_page(request):
+    message = request.session["message"]
+    return render(request, "error.html", {"message": message})
